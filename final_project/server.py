8c69e328b16fe89615615468a99ad44c705411c0
Rz108 <98604739+Rz108@users.noreply.github.com>
2022-01-30 00:49:07 +0800

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -23,7 +23,7 @@ def french_to_english():
 
 @app.route("/")
 def renderIndexPage():
-    return render_template("result_html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
