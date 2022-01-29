b6d6374e6df74cccd86eb1b1395c345d10c64a35
Rz108 <98604739+Rz108@users.noreply.github.com>
2022-01-30 00:31:03 +0800

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,35 +1,17 @@
-from machinetranslation import translator
-from flask import Flask, render_template, request
-import json
+    french_text=translator.english_to_french(textToTranslate)
+    return french_text
 
-app = Flask("Web Translator")
-
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    french_dict=language_translator.translate(
-        text=english_text,
-        model_id='en-fr'
-    ).get_result()
-    frenchtext = french_dict.get("translations")[0].get("translation")
-    return "Translated text to French"
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
+def french_to_english():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    english_dict=language_translator.translate(
-        text=french_text,
-        model_id='fr-en'
-    ).get_result()
-    englishtext=english_dict.get("translations")[0].get("translation")
-    return "Translated text to English"
+    english_text=translator.french_to_english(textToTranslate)
+    return english_text
+
 
 @app.route("/")
 def renderIndexPage():
-    textToTranslate=request.args.get('textToTranslate')
-    return render_template("result_html",username=textToTranslate)
+    return render_template("result_html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
